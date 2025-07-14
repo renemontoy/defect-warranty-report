b96a0d3b9db43cd720215e264d21ad15309a0784
renemontoy <renemontoy@gmail.com>
2025-07-13 20:23:20 -0700

    Act script

--- app.py
+++ app.py
@@ -493,7 +493,7 @@ def procesar_archivos(defectFile, productionFile, semana_seleccionada):
     avg_orders_data = [['Last 4 Weeks']]
     avg_orders_data += [list(avg_orders_pct.columns)]
     avg_orders_data += avg_orders_pct.values.tolist()
-    avg_weekly_pct = (sum_pct / len(week_cols))
+    avg_weekly_pct = orders_pct[week_cols].mean().sum()
     total_errors = sum_pct[week_cols].sum()
     avg_orders_data.append([
         f"{avg_weekly_pct.mean().round(1)}%",  
