d742f82a2e2a1630bec3ff501bcde85d3f0763e0
Rene Montoy <rivan@pxg.com>
2025-07-29 16:13:03 -0700

    Agregando titulos

--- app.py
+++ app.py
@@ -713,10 +713,9 @@ def procesar_archivos(defectFile, productionFile, semana_seleccionada):
         'Original Sales Order Date':'Build Date'
     }
     df_semana_actual = df_semana_actual.rename(columns=rename_columns)
-    df_semana_actual["Pod"] = df_semana_actual["Pod"].fillna("0")
     df_semana_actual["Pod"] = pd.to_numeric(df_semana_actual["Pod"], errors="coerce")
     df_semana_actual["Pod"] = df_semana_actual["Pod"].astype("Int64")
-    df_semana_actual["Pod"] = df_semana_actual["Pod"].replace(0,"-")
+    df_semana_actual["Pod"] = df_semana_actual["Pod"].astype(str).replace("<NA>", "-")
     df_semana_actual = df_semana_actual.sort_values(by="Type")
     df_semana_actual = df_semana_actual.fillna("-")
     semana_actual_data = [df_semana_actual.columns.tolist()]  # Encabezados
